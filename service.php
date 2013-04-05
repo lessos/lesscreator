38799b2d8362faef5c76d075262acfe409cd79b5
Eryx <evorui@gmail.com>
2013-04-06 02:33:27 +0800

    [NEW] const definitions added

--- service.php
+++ service.php
@@ -3,6 +3,17 @@
 
 class h5creator_service
 {
+    const ExecModeManual     = '1';
+    const ExecModeTime       = '2';
+    const ExecModeLoop       = '3';
+    const ExecModeForever    = '4';
+
+    const ParaModeDebug      = '1';
+    const ParaModeServer     = '2';
+    const ParaModeDataSingle = '3';
+    const ParaModeDataServer = '4';
+    const ParaModeDataShard  = '5';
+
     public static function debugPrint($d)
     {
         echo "<pre>";
@@ -22,21 +33,21 @@ class h5creator_service
     public static function listExecMode()
     {
         return array(
-            '1' => 'Execute by manual',
-            '2' => 'Execute on a regular time',
-            '3' => 'Execute by loop, after a certain time interval',
-            '4' => 'Execute forever, never expires',
+            self::ExecModeManual => 'Execute by manual',
+            self::ExecModeTime => 'Execute on a regular time',
+            self::ExecModeLoop => 'Execute by loop, after a certain time interval',
+            self::ExecModeForever => 'Execute forever, never expires',
         );
     }
 
     public static function listParaMode()
     {
         return array(
-            '1' => 'Develop or Debug, starts a single process',
-            '2' => 'Bound to server. Each physical server starts a process',
-            '3' => 'Bound to database. Each database starts a process',
-            '4' => 'Bound to database. Each physical server starts a process',
-            '5' => 'Bound to database. Each shard node starts a process',
+            //ParaModeDebug => 'Develop or Debug, starts a single process',
+            self::ParaModeServer => 'Bound to server. Each physical server starts a process',
+            self::ParaModeDataSingle => 'Bound to database. Each database starts a process',
+            self::ParaModeDataServer => 'Bound to database. Each physical server starts a process',
+            self::ParaModeDataShard => 'Bound to database. Each shard node starts a process',
         );
     }
 }
