636232420355d1982a94a42d22dfef915569fd25
Eryx <evorui@gmail.com>
2013-08-10 14:53:56 +0800

    [NEW] New lcData.* APIs based on html5.IndexedDB

--- static/js/gen.js
+++ static/js/gen.js
@@ -1,4 +1,142 @@
 
+//prefixes of implementation that we want to test
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+
+//prefixes of window.IDB objects
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+
+if (!window.indexedDB) {
+    window.alert("Your browser doesn't support a stable version of IndexedDB.")
+}
+
+var lcData = {};
+lcData.db = null;
+lcData.version = 9;
+lcData.schema = [
+    {
+        name: "files",
+        pri: "id",
+        idx: ["projdir"]
+    },
+    {
+        name: "config",
+        pri: "id",
+        idx: ["type"]
+    }
+];
+lcData.Init = function(dbname)
+{
+    var req = indexedDB.open(dbname, lcData.version);  
+
+    req.onsuccess = function (event) {
+        lcData.db = event.target.result;
+    };
+
+    req.onerror = function (event) {
+        console.log("IndexedDB error: " + event.target.errorCode);
+    };
+
+    req.onupgradeneeded = function (event) {
+        
+        lcData.db = event.target.result;
+
+        for (var i in lcData.schema) {
+            
+            var tbl = lcData.schema[i];
+            
+            if (lcData.db.objectStoreNames.contains(tbl.name)) {
+                lcData.db.deleteObjectStore(tbl.name);
+            }
+
+            var objectStore = lcData.db.createObjectStore(tbl.name, {pri: tbl.pri});
+
+            for (var j in tbl.idx) {
+                objectStore.createIndex(tbl.idx[j], tbl.idx[j], { unique: false });
+            }
+        }
+    };
+}
+
+lcData.Put = function(tbl, entry, cb)
+{    
+    if (lcData.db == null) {
+        return;
+    }
+
+    var req = lcData.db.transaction([tbl], "readwrite").objectStore(tbl).put(entry);
+
+    req.onsuccess = function(event) {
+        if (cb != null && cb != undefined) {
+            cb(true);
+        }
+    };
+
+    req.onerror = function(event) {
+        if (cb != null && cb != undefined) {
+            cb(false);
+        }
+    }
+}
+
+lcData.Get = function(tbl, key, cb)
+{
+    if (lcData.db == null) {
+        return;
+    }
+
+    var req = lcData.db.transaction([tbl]).objectStore(tbl).get(key);
+
+    req.onsuccess = function(event) {
+        cb(req.result);
+    };
+
+    req.onerror = function(event) {
+        cb(req.result);
+    }
+}
+
+lcData.Del = function(tbl, key, cb)
+{
+    if (lcData.db == null) {
+        return;
+    }
+
+    lcData.db.deleteDatabase("eryx");
+
+    var req = lcData.db.transaction([tbl], "readwrite").objectStore(tbl).delete(key);
+
+    req.onsuccess = function(event) {
+        cb(true);
+    };
+
+    req.onerror = function(event) {
+        cb(false);
+    }
+}
+
+lcData.List = function(tbl, cb)
+{
+    if (lcData.db == null) {
+        return;
+    }
+
+    var req = lcData.db.transaction([tbl], "readwrite").objectStore(tbl).openCursor();
+
+    req.onsuccess = function(event) {
+        var cursor = event.target.result;
+        if (cursor) {
+            cb(cursor);
+        }
+    };
+
+    req.onerror = function(event) {
+
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
 function h5cPluginDataOpen()
 {
     lessModalOpen('/lesscreator/data/open', 1, 700, 450, 
